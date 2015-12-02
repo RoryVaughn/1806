0157e36edf71a75603c325c7d52e33b1ef0fc257
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2015-12-01 18:00:37 -0600

    Python
    incomplete python assignment

---
+++ Python thing/Call.py
@@ -0,0 +1,11 @@
+import calculation
+from calculation import *
+list = [5,3,2]
+def test()
+print("hello world")
+n1 = 5
+n2 = 3
+n3 = add(n1,n2)
+print(n3)
+
+test();
---
+++ Python thing/test.py
@@ -0,0 +1,10 @@
+file = "Test.txt"
+inFile = open(file,'r')
+#print ("What is inFile?",inFile)
+count = 0
+for i in inFile:
+	count+=1
+	print(i)
+	
+		
+		
